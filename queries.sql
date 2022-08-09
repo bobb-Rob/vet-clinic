d4a48d0e9b787b181239c9ae10eca1c2ef833fa6
Robertson Arthur <robertsonakpan@gmail.com>
2022-08-09 22:36:04 +0100

    query and update animals table

--- queries.sql
+++ queries.sql
@@ -15,3 +15,70 @@ SELECT * FROM animals WHERE neutered=true;
 SELECT * FROM animals WHERE name='Gabumon';
 
 SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
+
+BEGIN;
+UPDATE animals SET species = 'unspecified';
+ROLLBACK;
+
+BEGIN;
+UPDATE animals SET species = 'digimon'
+WHERE name LIKE '%mon%';
+
+UPDATE animals SET species = 'pokemon'
+WHERE species IS NULL;
+COMMIT;
+
+SELECT name, species FROM animals;
+
+BEGIN;
+DELETE FROM animals;
+SELECT * FROM animals;
+ROLLBACK;
+
+BEGIN;
+ DELETE FROM animals
+ WHERE date_of_birth > '01-01-2022';
+ SAVEPOINT SP1;
+
+UPDATE animals
+SET weight_kg = weight_kg * -1;
+ROLLBACK TO SP1;
+
+UPDATE animals
+SET weight_kg = weight_kg * -1
+WHERE weight_kg < 0;
+COMMIT;
+
+SELECT count(*) FROM animals;
+
+SELECT count(*) FROM animals
+WHERE escape_attempts = 0;
+
+SELECT AVG(weight_kg)
+FROM animals;
+
+SELECT 
+  MAX(escape_attempts)
+FROM animals
+WHERE neutered = TRUE OR neutered = FASLE;
+
+SELECT   
+  MAX(weight_kg),
+  MIN(weight_kg)
+FROM animals
+WHERE species = 'digimon';
+
+SELECT   
+  MAX(weight_kg),
+  MIN(weight_kg)
+FROM animals
+WHERE species = 'pokemon';
+
+SELECT   
+  AVG(escape_attempts) ::numeric(10,2)
+FROM animals
+WHERE species like 'pokemon%' AND 
+date_of_birth BETWEEN '01-01-1990' AND '01-01-2000';
+
+
+
