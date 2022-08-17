07006b0760f89205b29f8d6e92e9a2de456aceb9
Robertson Arthur <robertsonakpan@gmail.com>
2022-08-17 07:35:30 +0100

    Created all tables FK index

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -1,23 +1,47 @@
-CREATE TABLE patients {
+CREATE TABLE patients (
  id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
  name VARCHAR(100),
- date_of_birth
-}
+ date_of_birth DATE
+);
 
 CREATE TABLE medical_histories (
   id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
   admitted_at timestamp,
   patient_id INT, 
   status VARCHAR,
-  FOREIGN KEY (patient_id) REFERENCES patients (id) ON DELETE CASCADE,
+  FOREIGN KEY (patient_id) REFERENCES patients (id) ON DELETE CASCADE
+);
+
+CREATE TABLE treatments (
+  id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  type VARCHAR,
+  name VARCHAR
+);
+
+CREATE TABLE treatments_medical_histories (
+  id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  treatment_id INT NOT NULL,  
+  medical_history_id INT NOT NULL,  
+  FOREIGN KEY (treatment_id) REFERENCES treatments (id) ON DELETE CASCADE,
+  FOREIGN KEY (medical_history_id) REFERENCES medical_histories (id) ON DELETE CASCADE
 );
 
 CREATE TABLE invoices (
   id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-  total_amount DECIMAL(2,12),
+  total_amount DECIMAL,
   generated_at timestamp,
   payed_at timestamp,
   medical_history_id INT,
-  FOREIGN KEY (medical_history_id) REFERENCES medical_histories (id) ON DELETE CASCADE,
+  FOREIGN KEY (medical_history_id) REFERENCES medical_histories (id) ON DELETE CASCADE
 );
 
+CREATE TABLE invoice_items (
+  id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  unit_price DECIMAL,
+  quantity INT,
+  total_price DECIMAL,
+  invoice_id INT,
+  treatment_id INT,
+  FOREIGN KEY (treatment_id) REFERENCES treatments (id) ON DELETE CASCADE,
+  FOREIGN KEY (invoice_id) REFERENCES invoices (id) ON DELETE CASCADE
+);
